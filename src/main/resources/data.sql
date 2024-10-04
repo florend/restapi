5c8cfeeccc29af390788d1a1db076c6779ebbdc0
florend <2990283+florend@users.noreply.github.com>
2024-10-04 11:46:42 +0200

    Added data.sql to populate schema created by Hibernate

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO post (title, body, date) VALUES
+    ('Title1', 'Body1', '2024-09-01'),
+    ('Title2', 'Body2', '2024-09-02'),
+    ('Title3', 'Body3', '2024-09-03'),
+    ('Title4', 'Body4', '2024-09-04'),
+    ('Title5', 'Body5', '2024-09-05');
